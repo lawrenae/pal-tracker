af78d7792ba6ec9c0fb1c50b20644e9eac2545c2
Andy <andy.lawrence@gmail.com>
2017-10-04 16:33:17 -0400

    Minor cleanup

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -1,17 +1,12 @@
 package io.pivotal.pal.tracker;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
-import java.sql.PreparedStatement;
 import java.sql.Date;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class JdbcTimeEntryRepository implements TimeEntryRepository {
@@ -73,18 +68,20 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
 
     @Override
     public TimeEntry update(Long id, TimeEntry timeEntry) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
         String sql = "update time_entries set project_id = ?, user_id = ?, date = ?, hours = ? where id = ?";
+
         template.update(
                 connection -> {
-                    PreparedStatement ps = connection.prepareStatement(sql, new String[]{"ID"});
+                    PreparedStatement ps = connection.prepareStatement(sql);
+
                     ps.setLong(1, timeEntry.getProjectId());
                     ps.setLong(2, timeEntry.getUserId());
                     ps.setDate(3, Date.valueOf(timeEntry.getDate()));
                     ps.setInt(4, timeEntry.getHours());
                     ps.setLong(5, id);
+
                     return ps;
-                }, keyHolder
+                }
         );
 
         timeEntry.setId(id);
